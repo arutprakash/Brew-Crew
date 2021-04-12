b3b71b7b5f96bc3eb12bf2b9528a689378a5db33
S Arut Prakash <arutprakash2000@gmail.com>
2021-04-12 19:12:32 +0530

    brew

---
+++ android/app/src/main/kotlin/m/f_brew_crew/MainActivity.kt
@@ -0,0 +1,6 @@
+package m.f_brew_crew
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/models/user.dart
@@ -0,0 +1,18 @@
+class User {
+
+  final String uid;
+
+  User({ this.uid });//constructor
+
+}
+
+class UserData {
+
+  final String uid;
+  final String name;
+  final String sugars;
+  final int strength;
+
+  UserData({this.uid,this.sugars,this.strength,this.name});
+
+}
